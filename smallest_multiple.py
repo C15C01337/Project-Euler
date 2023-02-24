7166c3284d731e6b7b3144051434b685af570acd
Bishal Aryal <bishal.dmarketer@gmail.com>
2023-02-24 16:38:25 +0545

    reduce execution time

--- smallest_multiple.py
+++ smallest_multiple.py
@@ -6,11 +6,11 @@ st = time.time()
 
 n = 20
 while n > 0:
-    if n % 20 == 0 and n % 19 == 0 and n % 18 == 0 and n % 17 == 0 and n % 16 == 0 and n % 15 == 0 and n % 14 == 0 and n % 14 == 0 and n % 13 == 0 and n % 12 == 0 and n % 11 == 0:
+    if n % 20 == 0 and n % 19 == 0 and n % 18 == 0 and n % 17 == 0 and n % 16 == 0 and n % 15 == 0 and n % 14 == 0  and n % 13 == 0 and n % 12 == 0 and n % 11 == 0:
         print(n)
         break
     else:
-        n = n + 1
+        n = n + 20
 
 et = time.time()
 print('Execution time:', float("{:.8f}".format(float(et - st))),'seconds')
