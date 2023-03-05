aacb2071487c8ac981b7cb4f202795f6df6a622c
Bishal Aryal <bishal.dmarketer@gmail.com>
2023-03-05 21:44:21 +0545

    Special Pythagorean triplet

---
+++ Special_Pythagorean_triplet.py
@@ -0,0 +1,20 @@
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+# a*2 + b*2 = c*2
+# For example, 3*2 + 4*2 = 9 + 16 = 25 = 5*2.
+
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+import time
+st = time.time()
+
+for a in range(1, 1000):
+    for b in range(a+1, 1000):
+        c = 1000 - a - b
+        if a*a + b*b == c*c:
+            print(a*b*c)
+            break
+
+et = time.time()
+print('Execution time:', float("{:.8f}".format(float(et - st))),'seconds')
