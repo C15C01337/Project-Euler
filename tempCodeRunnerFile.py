1bd92370c73dba6d2542720a4f6c4bbe5c031a05
Bishal Aryal <bishal.dmarketer@gmail.com>
2023-03-23 16:08:01 +0545

    largest product in grid

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,26 +1,5 @@
-# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-# Find the sum of all the primes below two million.
-import numpy
-import time
-st = time.time()
-
-
-# listing = [2]
-n = 3
-sum = 2
-while n < 2000000:
-    a = 2
-    for a in range(2, int(n**0.5+1)):
-        if n % a == 0:
-            break
-    if n % a != 0:
-        sum = sum + n
-        # listing.append(n)
-    n += 2
-     
-# print(listing)
-# print(numpy.sum(listing))
-
-print(sum)
-et = time.time()
-print('Execution time:', float("{:.8f}".format(float(et - st))),'seconds') 
+# Example
+# d = traverse((6, 8), 4, grid, (1, 1))
+# p = prod([x for x in d])
+# print('26 x 63 x 78 x 14 = ', 1788696, p == 1788696, '\nActual:', p) # [26, 63, 78, 14] 1788696
+# print()
