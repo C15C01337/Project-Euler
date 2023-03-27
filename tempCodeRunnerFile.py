07d439d5862a0b38199966c84361058f274c7a9a
Bishal Aryal <bishal.dmarketer@gmail.com>
2023-03-27 16:50:24 +0545

    large_sum

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-# Example
-# d = traverse((6, 8), 4, grid, (1, 1))
-# p = prod([x for x in d])
-# print('26 x 63 x 78 x 14 = ', 1788696, p == 1788696, '\nActual:', p) # [26, 63, 78, 14] 1788696
-# print()
