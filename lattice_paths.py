0e341b0d23ef9c398a0dea1b8520d2f32df30430
Bishal Aryal <bishal.dmarketer@gmail.com>
2023-04-03 16:52:56 +0545

    lattice paths

--- lattice_paths.py
+++ lattice_paths.py
@@ -2,3 +2,23 @@
 
 # How many such routes are there through a 20×20 grid?
 
+
+import time
+st = time.time()
+
+import math
+
+print('''# combinatorics to count the number of paths. 
+The number of paths from the top left corner to the bottom right corner of an n x n grid is given by
+the central binomial coefficient:
+
+(2n)C(n) = (2n)! / (n! * n!)
+For a 20 x 20 grid, this becomes:
+
+ (2 * 20)C(20) = (40!)/(20! * 20!)
+''')
+
+print(int(math.factorial(40)/(math.factorial(20)**2)))
+
+et = time.time()
+print('Execution time:', float("{:.8f}".format(float(et - st))),'seconds')
